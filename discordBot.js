526ee42d8356e7223f12062cd090b6cca223db41
PLAY <dafirstkingboys@gmail.com>
2019-01-24 22:08:16 -0500

    procfile

--- discordBot.js
+++ discordBot.js
@@ -7,7 +7,7 @@ const client = new Commando.Client({
    *  OWNER ID MOVED TO TOKEN.JS
    * ########################### */
   owner: process.env.DISCORD_OWNERID || require('./config/config.js').ownerId,
-  commandPrefix: "???",
+  commandPrefix: "&",
   unknownCommandResponse: false
 });
 global.featureLoader = new FeatureLoader(client);
